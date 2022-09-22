9c8d8187aebc468f76d4b703591de7932d7fffa2
Richard Neumann <mail@richard-neumann.de>
2022-09-23 00:52:07 +0200

    Directly call set_mine()

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,7 @@ use board::Board;
 
 fn main() {
     let mut board = Board::new(3, 4, 5);
-    let field = board.field(1, 1);
-    field.set_mine();
+    board.field(1, 1).set_mine();
 
     for mut field in board.fields() {
         println!("Field: {:#?}", field);
