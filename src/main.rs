b45314aca946ba3b0079c829c2af7a1307154d05
Richard Neumann <mail@richard-neumann.de>
2022-09-26 23:46:48 +0200

    Add error handling

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,14 @@ mod game;
 use game::Game;
 
 fn main() {
-    let mut game = Game::new(5, 4, 8).unwrap();
-    game.toggle_flag(1, 2);
-    game.visit(2, 2);
-    println!("Game: {:#?}", game);
+    let result = Game::new(5, 4, 8);
+
+    if result.is_err() {
+        println!("Error: {}", result.err().unwrap());
+    } else {
+        let mut game = result.unwrap();
+        game.toggle_flag(1, 2);
+        game.visit(2, 2);
+        println!("Game: {:#?}", game);
+    }
 }
