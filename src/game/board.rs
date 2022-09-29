f7b5b92a5d6b3a473c8f509f821eb594d17a5090
Richard Neumann <mail@richard-neumann.de>
2022-09-29 21:17:05 +0200

    Iteratively unvocer neighboring fields

--- src/game/board.rs
+++ src/game/board.rs
@@ -1,4 +1,5 @@
 use itertools::Itertools;
+use std::collections::HashMap;
 
 use grid::Grid;
 use rand::{seq::IteratorRandom, thread_rng};
@@ -85,12 +86,7 @@ impl Board {
             .for_each(|field| field.set_mine())
     }
 
-    fn initialize(&mut self) {
-        self.populate_mines();
-        self.initialized = true;
-    }
-
-    fn visit_recursive(&mut self, x: usize, y: usize) -> MoveResult {
+    fn visit_coordinate(&mut self, x: usize, y: usize) -> MoveResult {
         let optional_field = self.fields.get_mut(x, y);
 
         if !optional_field.is_ok() {
@@ -99,7 +95,7 @@ impl Board {
 
         let field = optional_field.unwrap();
 
-        if field.visited() {
+        if self.initialized && field.visited() {
             return MoveResult::AlreadyVisited;
         }
 
@@ -117,15 +113,31 @@ impl Board {
     }
 
     fn visit_neighbors(&mut self, x: usize, y: usize) {
-        if self.neighboring_mines(x, y) != 0 {
-            let mut positions_to_visit = Vec::new();
-            self.fields
-                .neighbors(x, y)
-                .filter(|(_, _, field)| !field.has_mine())
-                .for_each(|(x, y, _)| positions_to_visit.push((x, y)));
-            positions_to_visit
-                .into_iter()
-                .for_each(|(x, y)| _ = self.visit_recursive(x, y));
+        let mut neighbors = HashMap::new();
+        neighbors.insert((x, y), ());
+
+        loop {
+            let new_neighbors = neighbors
+                .iter()
+                .filter(|(&(x, y), _)| self.neighboring_mines(x, y) == 0)
+                .flat_map(|(&(x, y), _)| {
+                    self.fields.neighbors(x, y).filter(|(nx, ny, neighbor)| {
+                        !neighbor.has_mine() && !neighbors.contains_key(&(*nx, *ny))
+                    })
+                })
+                .collect_vec();
+
+            if new_neighbors.len() == 0 {
+                break;
+            }
+
+            for (x, y, _) in new_neighbors {
+                neighbors.insert((x, y), ());
+            }
+        }
+
+        for &(x, y) in neighbors.keys() {
+            self.fields.get_mut(x, y).unwrap().visit();
         }
     }
 
@@ -134,8 +146,9 @@ impl Board {
 
         if optional_field.is_ok() {
             optional_field.unwrap().visit();
-            self.initialize();
-            self.visit_neighbors(x, y);
+            self.populate_mines();
+            self.visit_coordinate(x, y);
+            self.initialized = true;
             MoveResult::Continue
         } else {
             MoveResult::InvalidPosition
@@ -146,7 +159,7 @@ impl Board {
         if !self.initialized {
             self.first_move(x, y)
         } else {
-            self.visit_recursive(x, y)
+            self.visit_coordinate(x, y)
         }
     }
 
