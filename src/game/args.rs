b30b8a891c0f48e9ff8ac49e78ca9c40cfb61624
Richard Neumann <mail@richard-neumann.de>
2022-10-30 22:08:47 +0100

    Change default grid size and mine count

--- src/game/args.rs
+++ src/game/args.rs
@@ -3,13 +3,13 @@ use clap::Parser;
 #[derive(Parser)]
 #[clap(about, author, version)]
 pub struct Args {
-    #[clap(short, long, name = "width", value_parser, default_value_t = 5)]
+    #[clap(short, long, name = "width", value_parser, default_value_t = 8)]
     pub width: usize,
 
-    #[clap(short = 'H', long, name = "height", value_parser, default_value_t = 5)]
+    #[clap(short = 'H', long, name = "height", value_parser, default_value_t = 8)]
     pub height: usize,
 
-    #[clap(short, long, name = "mines", value_parser, default_value_t = 8)]
+    #[clap(short, long, name = "mines", value_parser, default_value_t = 10)]
     pub mines: u8,
 
     #[clap(short, long, name = "duds", value_parser, default_value_t = 0)]
