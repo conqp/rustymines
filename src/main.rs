d35822a69d815a5b0fa56c7b9513d5e8a01a3c92
Richard Neumann <mail@richard-neumann.de>
2022-10-31 21:25:22 +0100

    Split main function into multiple functions

--- src/main.rs
+++ src/main.rs
@@ -13,30 +13,35 @@ fn main() {
     match Game::from_args() {
         Ok(mut game) => {
             println!("{}", game);
-            println!("Visit a field:                x y");
-            println!("Toggle flag on a field:       !x y");
-            println!("Visit all non-flagged fields: !!");
-            println!("Abort:                        exit");
-
-            while !game.over() {
-                match read::<String>("Enter action: ").trim() {
-                    "!!" => game.visit_unflagged_fields(),
-                    input => match Action::from_str(input) {
-                        Ok(action) => match action.kind() {
-                            ActionKind::Visit => game.visit(&action.coordinate().unwrap()),
-                            ActionKind::ToggleFlag => {
-                                game.toggle_flag(&action.coordinate().unwrap())
-                            }
-                            ActionKind::Exit => {
-                                println!("Bye!");
-                                break;
-                            }
-                        },
-                        Err(msg) => eprintln!("Error: {}", msg),
-                    },
-                }
-            }
+            print_help();
+            play_game(&mut game);
         }
         Err(msg) => eprintln!("Error: {}", msg),
     }
+}
+
+fn play_game(game: &mut Game) {
+    while !game.over() {
+        match read::<String>("Enter action: ").trim() {
+            "!!" => game.visit_unflagged_fields(),
+            input => match Action::from_str(input) {
+                Ok(action) => match action.kind() {
+                    ActionKind::Visit => game.visit(&action.coordinate().unwrap()),
+                    ActionKind::ToggleFlag => game.toggle_flag(&action.coordinate().unwrap()),
+                    ActionKind::Exit => {
+                        println!("Bye!");
+                        break;
+                    }
+                },
+                Err(msg) => eprintln!("Error: {}", msg),
+            },
+        }
+    }
+}
+
+fn print_help() {
+    println!("Visit a field:                x y");
+    println!("Toggle flag on a field:       !x y");
+    println!("Visit all non-flagged fields: !!");
+    println!("Abort:                        exit");
 }
