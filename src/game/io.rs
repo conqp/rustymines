f73663b2c88c3949d165e65584458cdf3f26de18
Richard Neumann <mail@richard-neumann.de>
2023-10-31 17:44:49 +0100

    rewrite read()

--- src/game/io.rs
+++ src/game/io.rs
@@ -7,32 +7,18 @@ where
     T: FromStr,
     <T as FromStr>::Err: Debug,
 {
-    loop {
-        match try_read::<T>(prompt) {
-            Err(msg) => eprintln!("Error: {msg}"),
-            Ok(value) => return value,
-        }
-    }
-}
-
-fn try_read<T>(prompt: &str) -> Result<T, &'static str>
-where
-    T: FromStr,
-    <T as FromStr>::Err: Debug,
-{
-    print_prompt(prompt);
     let mut input = String::new();
 
-    match std::io::stdin().read_line(&mut input) {
-        Ok(_) => input
-            .trim()
-            .parse::<T>()
-            .map_or_else(|_| Err("invalid value"), |value| Ok(value)),
-        Err(_) => Err("no value read"),
-    }
-}
+    loop {
+        print!("{prompt}");
+        std::io::stdout().flush().expect("Could not flush stdout.");
 
-fn print_prompt(prompt: &str) -> bool {
-    print!("{prompt}");
-    std::io::stdout().flush().is_ok()
+        if let Some(message) = std::io::stdin()
+            .read_line(&mut input)
+            .ok()
+            .and_then(|_| input.trim().parse::<T>().ok())
+        {
+            return message;
+        }
+    }
 }
