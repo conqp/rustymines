08907cfd48d0690c06feb5968289be8fba5d6531
Richard Neumann <mail@richard-neumann.de>
2024-08-05 18:05:23 +0200

    Refactor read()

--- src/game/io.rs
+++ src/game/io.rs
@@ -7,17 +7,14 @@ where
     T: FromStr,
     <T as FromStr>::Err: Debug,
 {
-    let mut input = String::new();
-
     loop {
-        input.clear();
         print!("{prompt}");
         std::io::stdout().flush().expect("Could not flush stdout.");
 
         if let Some(value) = std::io::stdin()
-            .read_line(&mut input)
-            .ok()
-            .and_then(|_| input.trim().parse::<T>().ok())
+            .lines()
+            .find_map(Result::ok)
+            .and_then(|line| line.trim().parse::<T>().ok())
         {
             return value;
         }
