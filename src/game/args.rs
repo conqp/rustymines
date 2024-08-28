b6eeab13a05e334e125251bb86219c30f38d36e0
Richard Neumann <mail@richard-neumann.de>
2024-08-28 08:57:04 +0200

    Remove excess newlines

--- src/game/args.rs
+++ src/game/args.rs
@@ -5,13 +5,10 @@ use clap::Parser;
 pub struct Args {
     #[clap(short, long, name = "width", value_parser, default_value_t = 8)]
     pub width: usize,
-
     #[clap(short = 'H', long, name = "height", value_parser, default_value_t = 8)]
     pub height: usize,
-
     #[clap(short, long, name = "mines", value_parser, default_value_t = 10)]
     pub mines: u8,
-
     #[clap(short, long, name = "duds", value_parser, default_value_t = 0)]
     pub duds: u8,
 }
