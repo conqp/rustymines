5cbbf9e59b95672d60ea6de90ec8578c1a722085
Richard Neumann <mail@richard-neumann.de>
2025-04-09 14:47:06 +0200

    Improve input parsing

--- src/game/io.rs
+++ src/game/io.rs
@@ -1,18 +1,65 @@
+use std::error::Error;
+use std::fmt::Display;
 use std::io::{stdin, stdout, Write};
 use std::str::FromStr;
+
+use log::error;
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq, Hash)]
+pub enum ReadError<T> {
+    InvalidInput,
+    ParseError(T),
+}
+
+impl<T> Display for ReadError<T>
+where
+    T: Display,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::InvalidInput => write!(f, "Invalid input"),
+            Self::ParseError(error) => write!(f, "Parse error: {error}"),
+        }
+    }
+}
+
+impl<T> Error for ReadError<T>
+where
+    T: Error + 'static,
+{
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        match self {
+            Self::ParseError(error) => Some(error),
+            Self::InvalidInput => None,
+        }
+    }
+}
+
+pub fn try_read<T>(prompt: &str) -> Result<T, ReadError<<T as FromStr>::Err>>
+where
+    T: FromStr,
+{
+    print!("{prompt}");
+    stdout()
+        .flush()
+        .expect("Could not flush stdout. This is a bug.");
+
+    let Some(value) = stdin().lines().find_map(Result::ok) else {
+        return Err(ReadError::InvalidInput);
+    };
+
+    value.trim().parse::<T>().map_err(ReadError::ParseError)
+}
 
 pub fn read_until_valid<T>(prompt: &str) -> T
 where
     T: FromStr,
+    <T as FromStr>::Err: Display,
 {
     loop {
-        print!("{prompt}");
-        stdout().flush().expect("Could not flush stdout.");
-
-        let value = stdin().lines().find_map(Result::ok);
-
-        if let Some(value) = value.and_then(|line| line.trim().parse::<T>().ok()) {
-            return value;
+        match try_read(prompt) {
+            Ok(value) => return value,
+            Err(error) => error!("{error}"),
         }
     }
 }
