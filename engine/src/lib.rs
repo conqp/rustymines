b196f49d8be4886effd5da8dd7273eb5d84da396
Richard Neumann <mail@richard-neumann.de>
2025-08-03 11:33:21 +0200

    Cargo fmt.

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -1,10 +1,10 @@
 //! A mine sweeping game engine.
 
+pub use game::Game;
 pub use game::action::Action;
+pub use game::board::Board;
 pub use game::board::error::Error;
 pub use game::board::field::{Field, View};
-pub use game::board::Board;
 pub use game::state::State;
-pub use game::Game;
 
 mod game;
